0ff290619774eb5738c75e1b74b6ec6e046e520f
Hubert Wang <hubert.tatra@gmail.com>
2020-08-20 14:36:33 +0700

    Add prometheus monitoring

--- gin/routes.go
+++ gin/routes.go
@@ -1,10 +1,15 @@
 package gin
 
-import "wang.hihubert.personal-backend/routes"
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"wang.hihubert.personal-backend/routes"
+)
 
 func initHandlerGroups() {
 	registerTestRoutes()
 	registerContactsRoutes()
+	registerMonitoringRoutes()
 }
 
 func registerTestRoutes() {
@@ -17,4 +22,10 @@ func registerContactsRoutes() {
 	contactRoutes := router.Group("/contacts")
 
 	contactRoutes.POST("/me", routes.SendMeMessage)
+}
+
+func registerMonitoringRoutes() {
+	promeRoutes := router.Group("/monitors")
+
+	promeRoutes.GET("/prometheus", gin.WrapH(promhttp.Handler()))
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,6 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/joho/godotenv v1.3.0
 	github.com/mailgun/mailgun-go/v4 v4.1.3
+	github.com/prometheus/client_golang v1.7.1
+	github.com/prometheus/common v0.10.0
 )
