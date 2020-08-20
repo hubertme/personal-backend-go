1316d033e8cb32580846fd95ece8ad5d65331f92
Hubert Wang <hubert.tatra@gmail.com>
2020-08-20 21:26:26 +0700

    Add CORS middleware

--- gin/gin.go
+++ gin/gin.go
@@ -3,10 +3,12 @@ package gin
 import "github.com/gin-gonic/gin"
 
 const LOCAL_ADDRESS = "localhost:3030"
+
 var router = gin.Default()
 
 func SetupGinServer() error {
 	//gin.SetMode(gin.ReleaseMode)
+	router.Use(allowCORSMiddleware())
 	initHandlerGroups()
 
 	return router.Run(LOCAL_ADDRESS)
